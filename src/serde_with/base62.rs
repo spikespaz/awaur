a8893b7f51f14f9f3d5f8b7639f04562f1298c90
Jacob Birkett <jacob@birkett.dev>
2022-06-23 19:03:42 -0700

    Add roundtrip test for Base62 wrapper

--- src/serde_with/base62.rs
+++ src/serde_with/base62.rs
@@ -53,3 +53,54 @@ where
         Base62::deserialize(deserializer).map(|this| this.0)
     }
 }
+
+#[cfg(test)]
+pub mod tests {
+    use serde::{Deserialize, Serialize};
+    use serde_with::serde_as;
+
+    use crate::serde_with::ext::Base62;
+
+    #[serde_as]
+    #[derive(Serialize, Deserialize)]
+    struct TestType<T>
+    where
+        T: Clone + TryFrom<u128> + Into<u128>,
+    {
+        #[serde_as(as = "Vec<Base62<T>>")]
+        pub values: Vec<T>,
+    }
+
+    // Test both serializing and deserializing in one go
+    #[test]
+    fn test_roundtrip() {
+        // Create a thousand large numbers, and add a zero for pedanticism
+        let range = ((u128::MAX - 1000)..u128::MAX).chain([0]);
+        // Encode those without the wrapper
+        let values = range.clone().map(base62::encode);
+        // Create an instance of the wrapper type with the same numbers
+        let container = TestType {
+            values: range.collect(),
+        };
+        // Serialize the value as a JSON string
+        let serialized = serde_json::to_string(&container).unwrap();
+        // Parse the string back into a `Value` type
+        let parsed = serde_json::from_str::<serde_json::Value>(&serialized).unwrap();
+        // Keep unwrapping to get the vector of encoded strings
+        let parsed = parsed
+            .as_object()
+            .unwrap()
+            .get("values")
+            .unwrap()
+            .as_array()
+            .unwrap()
+            .iter()
+            .map(|v| v.as_str().unwrap());
+
+        // Iterate over the manually encoded numbers and the ones encoded by the
+        // wrapper's implementation of `SerializeAs` and `Serialize`
+        for (expect, actual) in std::iter::zip(values, parsed) {
+            assert_eq!(&expect, &actual);
+        }
+    }
+}
