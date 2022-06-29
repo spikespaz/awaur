ddf1c0c335d15e9e88a5a6c51ae529d4b5550867
Jacob Birkett <jacob@birkett.dev>
2022-06-29 15:42:01 -0700

    Add endpoint macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 #![warn(missing_docs)]
 #![doc = include_str!("../README.md")]
 
+#[cfg(feature = "endpoints")]
+pub mod endpoints;
 pub mod macros;
 #[cfg(feature = "paginator")]
 pub mod paginator;
